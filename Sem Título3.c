ea1c7c40963443a1c347e0cc81b03eeff46d82ef
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-03-15 11:32:10 -0300

    Add files via upload

---
+++ Sem Título3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+	// Declaração variáveis.
+	char nome[200]; // Cadeia
+	char sexo; // Caracter
+	int idade; // Inteiro
+	float peso; // Real
+	
+	// Solicitando dados para o usuário.
+	printf("Digite seu nome: ");
+	scanf("%s",&nome);
+	
+	fflush(stdin); // Limpa o cache de input.
+	
+	printf("Digite seu sexo - M ou F: ");
+	scanf("%c",&sexo);
+	
+	printf("Digite sua idade: ");
+	scanf("%i",&idade);
+	
+	printf("Digite seu peso: ");
+	scanf("%f",&peso);
+	
+	// Exibindo resultados.
+	printf("\n=== Exibindo resultados ===\n");
+	printf("Nome: %s \n", nome); // %s -> string
+	printf("Sexo: %c \n", sexo); // %c -> char
+	printf("Idade: %d \n", idade); // %i -> int
+	printf("Peso: %f \n", peso); // %f -> float
+	
+	getchar();
+	
+	return 0;
+
+ }
