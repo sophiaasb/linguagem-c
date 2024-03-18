66bace6d139d611ac2d9a8e030b1c960569e098b
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-03-18 11:12:21 -0300

    Add files via upload

---
+++ c.07.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	atelocale(LC_ALL, "");
+	
+	float valor, desconto
+	
+	printf("Digite um valor: ");
+	scanf("%f",&valor);
+	
+	desconto = valor - (valor * 0.1);
+	valorFinal = valor - desconto
+	
+	system("cls || clear"); 
+	
+	printf("\n=== Exibindo resultados ===\n");
+	printf("Valor informado: R$ %.2f \n", valor);
+	printf("Desconto: %.2f \n", desconto):
+	printf("Valor final: %.2f \n", valorFinal);
+	
+	return 0;
+	
+}
