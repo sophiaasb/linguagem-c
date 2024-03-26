82343cb0305dbcbb79b29d950d584bcaa4da6d47
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-03-26 10:59:18 -0300

    Add files via upload

---
+++ Sem Título5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+int main() {
+	setlocale(LC_ALL, "");
+	
+	char resultado[200];
+    float nota, media, soma = 0;
+    int i;
+	
+	for (i = 1; i <= 3; i++) {
+        do {	
+		printf("Digite a %iª nota: ", i);
+		scanf("%f",&nota);
+	} while (nota < 0 || nota > 10);
+	
+    soma += nota;
+    
+}
+
+    media = soma / 2;
+    
+    if (media >= 7) {
+    strcpy(resultado, "Aprovado.");
+   } else if (media >= 5) {
+    strcpy(resultado, "Recuperação.");
+   } else {
+    strcpy(resultado, "Reprovado.");
+   }
+    
+    printf("\n === Exibindo resultado === \n");
+    printf("Média: %.1f \n", media);
+    printf("Resultado: %s \n", resultado);
+
+	return 0;
+	
+}
