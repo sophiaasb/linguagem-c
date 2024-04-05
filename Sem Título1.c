4989edf3a3a70af1dfd8174e38318cfb18353a6c
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-04-05 10:21:02 -0300

    Add files via upload

---
+++ Sem Título1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main() {
+	setlocale(LC_ALL, "portuguese");
+	
+	int opcao, contadorSalario = 0, mulheres5k = 0;
+	int idade, maiorIdade = INT_MIN, menorIdade = INT_MAX;
+	float salario, somaSalario = 0, mediaSalario;
+	char sexo;
+	
+	do { 
+	printf("=== MENU === \n");
+	printf("1 - Adicionar pessoa \n");
+    printf("2 - Exibir resultado e sair \n");
+    printf("Digite a opção desejada: ");
+    scanf("%i", &opcao);
+	
+	switch (opcao) {
+	    case 1:
+		printf("Digite a idade: ");
+	    scanf("%i", &idade);
+	
+	    fflush(stdin);
+	    printf("Digite o sexo - M ou F: ");
+	    scanf("%s", &sexo);
+	
+	    printf("Digite o salário: ");
+	    scanf("%f", &salario);
+	
+	    sexo = toupper(sexo);
+	
+	    if (idade > maiorIdade) {
+		    menorIdade = idade;
+	    }
+	    
+	    if (idade > menorIdade) {
+		menorIdade = idade;
+     	}
+	
+	    contadorSalario++;
+	    somaSalario += salario;
+	
+	    if (sexo == 'F' && salario >= 5000) {
+		    mulheres5k++;
+	}
+	
+	      break;
+	  case 2:
+		  break;
+	  default:
+		 printf("\nOpção inválida \n");
+		 sleep(4);
+		 system("cls || clear");
+		
+        }
+    } while (opcao != 2);
+
+    mediaSalario = somaSalario / contadorSalario;  
+   		
+    printf("=== Exibindo resultados ===\n");
+    printf("Média salarial do grupo: %.2f \n", mediaSalario);
+    printf("Maior idade: %i \n", maiorIdade);
+    printf("Menor idade: %i \n", menorIdade);
+   		
+    return 0;
+   
+}
