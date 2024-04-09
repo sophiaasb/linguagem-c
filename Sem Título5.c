850b6546a605942341b0c70d5777e8eba0a91ee0
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-04-09 10:17:04 -0300

    Add files via upload

---
+++ Sem Título5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+#define TAM 3
+
+int main() {
+	setlocale(LC_ALL, "");
+	
+	float notas[TAM], soma = 0, media;
+	int i;
+	
+	printf("Digite as três notas: \n");
+	for (i = 0; i < TAM; i++) {
+		printf("Digite a %dª nota: ", i + 1);
+		scanf("%f", &notas[i]);
+		
+		soma += notas[i];
+	}
+	
+	media = soma / (float) TAM;
+	
+	printf("\nExibindo as notas informadas: \n");
+	for (i = 0; i < TAM; i++) {
+		printf("%dª nota: %.1f \n", i+1, notas[i]);
+	}
+	
+	printf("\nMédia: %.1f \n", &media);
+	
+	return 0;
+}
