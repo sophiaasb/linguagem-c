b382781347a82357ac8a5316dab6418e1814fa85
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-04-09 11:30:06 -0300

    Add files via upload

---
+++ Sem Título6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <locale.h> 
+#define TAM 4
+
+int main() {
+	setlocale(LC_ALL, "");
+	
+	float notas[TAM], soma = 0, media;
+	int i;
+	
+	printf("Digite as três notas: \n");
+	for (i = 0; i < TAM; i++) {
+		printf("Digite a %dª nota: ", i + 1);
+		scanf("%f", &notas[i]);
+		
+		soma += notas[i];
+	}
+	
+	media = soma / (float) TAM;
+	
+	printf("\nExibindo as notas informadas: \n");
+	for (i = 0; i < TAM; i++) {
+		printf("%dª nota: %.1f \n", i+1, notas[i]);
+	}
+	
+	printf("\nMédia: %.1f \n", media);
+	
+	if (media >= 7) printf("\nAprovado!");
+    if (media < 7) printf("\nEm recuperação.");
+    if (media < 5) printf("\nReprovado.");
+	
+	return 0;
+}
---
+++ Sem Título7.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <locale.h>
+#include <limits.h>
+
+#define TAM 5
+
+   int main() {
+   setlocale(LC_ALL, "");
+
+   int numeros[TAM];
+   int maiorNumero = INT_MIN;
+   int menorNumero = INT_MAX;
+   int i;
+
+   printf("Digite os numeros solicitados:\n");
+   for (i = 0; i < TAM; i++) {
+    printf("Digite o %dº mumero: ", i + 1);
+    scanf("%i", &numeros[i]);
+
+   if (numeros[i] > maiorNumero) {
+   maiorNumero = numeros[i];
+   }
+   if (numeros[i] < menorNumero) {
+   menorNumero = numeros[i];
+
+   if (numeros[i] < menorNumero) {
+   menorNumero = numeros[i];
+   }
+ }
+
+   printf("\nExibindo os numeros informados:\n");
+   for (i = 0; i < TAM; i++) {
+   printf("%dº numero: %i \n", i + 1, numeros[i]);
+   }
+
+   printf("\nMaior número: %i \n", maiorNumero);
+   printf("Menor número: %i \n", menorNumero);
+
+   return 0;
+
+}
