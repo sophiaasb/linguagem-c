ea6c8c650b83375862c96ef4277f783816ff0ab8
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-04-09 10:20:16 -0300

    Update Sem Título5.c

--- Sem Título5.c
+++ Sem Título5.c
@@ -8,9 +8,9 @@ int main() {
 	float notas[TAM], soma = 0, media;
 	int i;
 	
-	printf("Digite as três notas: \n");
+	printf("Digite as trÃªs notas: \n");
 	for (i = 0; i < TAM; i++) {
-		printf("Digite a %dª nota: ", i + 1);
+		printf("Digite a %dÂª nota: ", i + 1);
 		scanf("%f", &notas[i]);
 		
 		soma += notas[i];
@@ -20,10 +20,10 @@ int main() {
 	
 	printf("\nExibindo as notas informadas: \n");
 	for (i = 0; i < TAM; i++) {
-		printf("%dª nota: %.1f \n", i+1, notas[i]);
+		printf("%dÂª nota: %.1f \n", i+1, notas[i]);
 	}
 	
-	printf("\nMédia: %.1f \n", &media);
+	printf("\nMÃ©dia: %.1f \n", media);
 	
 	return 0;
 }
