51f98b4857cfd5a2b364886af56b2d29547c9fb4
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-04-12 08:16:07 -0300

    Create vetores8.c

---
+++ vetores8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <limits.h>
+#define TAM 5
+
+int main() {
+	setlocale(LC_ALL, "portuguese");
+	
+	int numeros[TAM];
+	int pares;
+	int impares;
+	int i;
+	
+	printf("Digite os números solicitados: \n")
+	for (i = 0; i < TAM; i++)
+}
